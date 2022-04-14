8d2feb9a39bb02b916cee97dcfa0ce2a8b6ec00f
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-04-13 21:02:00 -0300

    Modified DataBase connection, Adding Pattern Singleton

--- connect/bdutil.py
+++ connect/bdutil.py
@@ -1,18 +1,32 @@
 import psycopg2
 
-import psycopg2
+class ConnectBD:
 
-class ConnectBD():
-    def __init__(self):
-        self.__connect = None
+  _instace = None
 
-    def get_connect(self):
-        if self.__connect is None:
-            self.__connect = psycopg2.connect(
-                host='localhost',
-                database='studyplan',
-                user='postgres',
-                password='postgres'
-            )
-        return self.__connect
+  # Método para se conectar no Banco do Postgres, utilizando a biblioteca Psycopg2.
+  def __init__(self):
+    self._connect = psycopg2.connect(
+      host="localhost",
+      database="studyplan",
+      user="postgres",
+      password="Lu274235Ely5"
+    )
 
+  # Utilizado o classMethod para ser possível criar um atributo metodo da classe
+  # Para não criar várias instancias do mesmo Objeto quando se for fazer uma conexão no banco
+  @classmethod
+  def get_connect(cls):
+    if cls._instace is None:
+      cls._instace = super().__new__(cls)
+    
+    return cls._instace
+  
+  '''
+  # Outra forma de fazer o Singleton - Sobreescrevendo o método __new__()
+  def __new__(cls, *args, **kwargs):
+    if cls._instace is None:
+      cls._instace = super().__new__(cls)
+    
+    return cls._instace
+  '''
