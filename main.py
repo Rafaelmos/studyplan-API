0d3e0d74da7a96e5e1cbf02996f1a6738bae7a51
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-04-15 17:09:21 -0300

    APP main.py

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-import string
 from flask import Flask
 
-##from connect.bdutil import ConnectBD
-##from module.curso.dao import CursoDao
+#Importando arquivos das Rotas alternativas:
+from module.usuario.controller import app_usuario 
 
-##app = Flask('')
 
-'''
-@app.route("/usuario/")
-def get_all_user():
-    user_dao = UserDao(ConnectBD().get_connect())
-    lista_user = user_dao.get_all()
-    return {
-            'status': True,
-            'objects': [curso.get_json() for curso in lista_cursos]
-        }
-
-@app.route("/curso/<int:id>/")
-def get_curso(id):
-    curso_dao = CursoDao(ConnectBD().get_connect())
-    curso = curso_dao.get_curso(id)
-    if curso:
-        return curso.get_json()
-    return {}
-'''
 app = Flask(__name__)
+app.register_blueprint(app_usuario)
+
 
 @app.route("/")
 def get_user():
   return "Hello world"
 
-app.run()
+if __name__ == '__main__':
+  app.run()
