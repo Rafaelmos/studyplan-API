ddda4a5003b4bf91c8c0aabcc896e932c973942e
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-04-15 18:08:34 -0300

    APP main.py

--- main.py
+++ main.py
@@ -3,14 +3,8 @@ from flask import Flask
 #Importando arquivos das Rotas alternativas:
 from module.usuario.controller import app_usuario 
 
-
 app = Flask(__name__)
 app.register_blueprint(app_usuario)
 
-
-@app.route("/")
-def get_user():
-  return "Hello world"
-
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
