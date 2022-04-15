45f13c44aa46c2143623bda281a7374b7735c383
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-04-15 17:10:12 -0300

    Class Routes Controller

--- module/usuario/controller.py
+++ module/usuario/controller.py
@@ -1,8 +1,15 @@
-from flask import Flask
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, make_response, Response
+from connect.bdutil import ConnectDataBase
+
+from module.usuario.dao import UsuarioDao
+from module.usuario.model import Usuario
+
+import traceback
 
 app_usuario = Blueprint("app_usuario", __name__)
 app_name = "usuario"
+
+dao = UsuarioDao(connectDataBase=ConnectDataBase())
 
 # Rota para listar todos os usuarios
 @app_usuario.route('/{}s/'.format(app_name))
@@ -17,10 +24,22 @@ def get_Usuario():
 
 
 # Rota para adicionaar novos usuarios
-@app_usuario.route('/{}/adicionar/'.format(app_name))
-def new_Usuario():
-  return 'Novo Usuario'
+@app_usuario.route('/{}/adicionar/'.format(app_name), methods=['POST'])
+def add_Usuario():
+  data = request.form.to_dict(flat=True)
+  usuario = None
+  try:
+    usuario = Usuario(**data)
+    usuario = dao.save(usuario)
+  except Exception as e:
+    print(traceback.format_exc())
+    return make_response(
+      {
+        'error': True,
+        'message': 'Verifique se os campos foram preenchidos corretamente'
+      }, 400)
 
+  return make_response({'id': usuario.id}, 201)
 
 # Rota para deletar algum usuario
 @app_usuario.route('/{}/delete/'.format(app_name))
