0406abb10b3921a239c79b694b694db4b49f7561
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-04-17 16:29:28 -0300

    Adding Router Lembretes

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 #Importando arquivos das Rotas alternativas:
 from module.usuario.controller import app_usuario 
+from module.lembretes.controller import app_lembretes
 
 app = Flask(__name__)
 app.register_blueprint(app_usuario)
+app.register_blueprint(app_lembretes)
 
 if __name__ == '__main__':
   app.run(debug=True)
