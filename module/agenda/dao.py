4dd863d624600e62bb67040e62835ae62eb36a12
BeatrizSilva-Dev <beatryz.a.silva2011@gmail.com>
2022-04-23 15:26:04 -0300

    Update dao.py

--- module/agenda/dao.py
+++ module/agenda/dao.py
@@ -2,6 +2,9 @@ import psycopg2
 
 SCRIPT_SQL_INSERT = 'INSERT INTO AGENDA(usuario_id) values(%s) returning id'
 SCRIPT_SQL_SELECT_ALL_AGENDAS = 'SELECT * FROM AGENDAS'
+#SELECT * FROM  cronogramadematerias as z WHERE z.agenda_id= 1
+#SELECT * FROM  metas as x WHERE x.agenda_id= 1
+#SELECT * FROM  lembretes as y WHERE y.agenda_id= 1
 
 class AgendaDao:
   def __init__(self, connectDataBase):
