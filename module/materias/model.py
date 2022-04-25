34a907bf360cc0a930517fe05679e0c62c06fe51
Rafaelmos <rafael.rjms0@gmail.com>
2022-04-25 15:21:07 -0300

    Update model

--- module/materias/model.py
+++ module/materias/model.py
@@ -1,15 +1,16 @@
 class Materias:
-    def __init__(self, materia, area):
+    def __init__(self, materia, area, cronogramadematerias_id):
         self.materia = materia
         self.area = area
+        self.cronogramadematerias_id = cronogramadematerias_id
         self.id = None
 
     def set_id(self, id):
         self.id = id
 
     def __str__(self):
-        return (f'Materia: {self.materia} - area: {self.area}')
+        return (f'Materia: {self.materia} - area: {self.area} - Cronograma: {self.cronogramadematerias_id}')
 
     def get_values_saves(self):
-            return [self.materia, self.area]
+            return [self.materia, self.area, self.cronogramadematerias_id]
 
