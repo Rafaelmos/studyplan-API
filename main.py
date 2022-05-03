8bbd165c08872c59304e1686ee10f36694d77174
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-05-02 22:35:53 -0300

    APP Main

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from module.agenda.controller import app_agenda
 from module.cronogramadematerias.controller import app_cronogramas
 from module.linksuteis.controller import app_links
 
-
-
 app = Flask(__name__)
 app.register_blueprint(app_usuario)
 app.register_blueprint(app_lembretes)
@@ -19,8 +17,6 @@ app.register_blueprint(app_materias)
 app.register_blueprint(app_agenda)
 app.register_blueprint(app_cronogramas)
 app.register_blueprint(app_links)
-
-
 
 if __name__ == '__main__':
   app.run(debug=True)
