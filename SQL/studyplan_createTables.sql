36dcf295db28b25bd94bd55c57beaec7994d9a57
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-05-08 16:17:52 -0300

    UPDATE SQL

--- SQL/studyplan_createTables.sql
+++ SQL/studyplan_createTables.sql
@@ -5,6 +5,14 @@ create table if not exists usuario(
 	idade integer
 );
 
+create table if not exists materias(
+	id serial primary key,
+	materia varchar(20),
+	area varchar(20),
+	usuario_id integer,
+	foreign key(usuario_id) references usuario(id)
+);
+
 create table if not exists agenda(
 	id serial primary key,
 	usuario_id integer,
@@ -13,21 +21,14 @@ create table if not exists agenda(
 
 create table if not exists cronogramadematerias(
 	id serial primary key,
-	materia varchar(20),
 	descricao varchar(255),
 	data timestamp,
 	agenda_id integer,
 	usuario_id integer,
-  foreign key(agenda_id) references agenda(id),
-  foreign key(usuario_id) references usuario(id)
-);
-
-create table if not exists materias(
-	id serial primary key,
-	materia varchar(20),
-	area varchar(20),
-	cronogramadematerias_id integer,
-	foreign key(cronogramadematerias_id) references cronogramadematerias(id)
+	materia_id integer,
+  	foreign key(agenda_id) references agenda(id),
+  	foreign key(usuario_id) references usuario(id),
+	foreign key(materia_id) references materias(id)
 );
 
 create table if not exists lembretes(
